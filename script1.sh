6c97250fa0ae20be1145128280be16d55f58890d
kaacyberit <kaacyberit@gmail.com>
2025-06-26 17:40:55 +0200

    third job

--- script1.sh
+++ script1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 uname -a
-echo "Hello from second job!"
+echo "Hello from third job!"
 echo "$param1"
 RESULT=$((num1 + num2))
 echo $RESULT
-echo "text to file from second Job - relative path" >> file.txt
+echo "text to file from third Job - relative path" >> file.txt
